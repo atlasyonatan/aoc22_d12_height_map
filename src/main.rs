e57fdf19e7ab8c09da052ef18da92a2780b27b8e
Yonatan Atlas <atlasyonatan@gmail.com>
2022-12-24 21:02:59 +0200

    finished part 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ndarray::{self, Array2};
-use std::collections::HashSet;
+use std::collections::{hash_map::Entry, HashMap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -22,15 +22,15 @@ fn main() {
     let (start, end) = {
         let mut start = Vec::new();
         let mut end = Vec::new();
-        for (p, c) in grid.indexed_iter_mut() {
-            match c {
+        for (position, character) in grid.indexed_iter_mut() {
+            match character {
                 'S' => {
-                    *c = 'a';
-                    start.push(p);
+                    *character = 'a';
+                    start.push(position);
                 }
                 'E' => {
-                    *c = 'z';
-                    end.push(p);
+                    *character = 'z';
+                    end.push(position);
                 }
                 _ => (),
             }
@@ -52,39 +52,49 @@ fn main() {
     }
 
     let count = shape.0 * shape.1;
-    let adjecancy = Array2::from_shape_fn((count, count), |(a, b)| -> usize {
+    let adjecancy = Array2::from_shape_fn((count, count), |(a, b)| {
         let (x1, y1) = (a % shape.0, a / shape.0);
         let (x2, y2) = (b % shape.0, b / shape.0);
         let step = (x1 as i32 - x2 as i32, y1 as i32 - y2 as i32);
         if steps.contains(&step) && can_traverse(grid[[x1, y1]], grid[[x2, y2]]) {
-            1
+            true
         } else {
-            0
+            false
         }
     });
 
-    let mut adj_pow = Array2::from_diag_elem(count, 1);
-    let start_p = start.1 * shape.0 + start.0;
-    let end_p = end.1 * shape.0 + end.0;
+    let start_node = start.1 * shape.0 + start.0;
+    let end_node = end.1 * shape.0 + end.0;
 
-    let mut len = 1;
-    while len <= count {
-        adj_pow = adj_pow.dot(&adjecancy);
-        if adj_pow[[start_p, end_p]] > 1 {
-            break;
+    match meet_in_the_middle(adjecancy, start_node, end_node) {
+        Some((middle, lengths)) => {
+            let total_length = lengths.iter().sum::<usize>();
+            println!("Found a shortest path of length {}", total_length);
+            let (x, y) = (middle % shape.0, middle / shape.0);
+            println!("Path mid point (x: {}, y: {})", x, y);
         }
-        len += 1;
+        None => println!("No path"),
     }
-    if len > count {
-        println!("Found no paths between start and end");
-    } else {
-        let paths = adj_pow[[start_p, end_p]];
-        let plural = if paths == 1 { "s" } else { "" };
-        println!(
-            "Found {} shortest path{} from start to end with length={} steps",
-            paths, plural, len
-        );
-    }
+
+    // let mut adj_pow = Array2::from_diag_elem(count, 1);
+    // let mut len = 1;
+    // while len <= count {
+    //     adj_pow = adj_pow.dot(&adjecancy);
+    //     if adj_pow[[start_p, end_p]] > 1 {
+    //         break;
+    //     }
+    //     len += 1;
+    // }
+    // if len > count {
+    //     println!("Found no paths between start and end");
+    // } else {
+    //     let paths = adj_pow[[start_p, end_p]];
+    //     let plural = if paths == 1 { "s" } else { "" };
+    //     println!(
+    //         "Found {} shortest path{} from start to end with length={} steps",
+    //         paths, plural, len
+    //     );
+    // }
 }
 
 fn array2_from_vec2<T>(mut input_vec2: Vec<Vec<T>>) -> Array2<T> {
@@ -99,4 +109,54 @@ fn array2_from_vec2<T>(mut input_vec2: Vec<Vec<T>>) -> Array2<T> {
     }
 
     Array2::from_shape_vec((nrows, ncols), data).unwrap()
+}
+
+fn meet_in_the_middle(
+    adjecancy: Array2<bool>,
+    from_node: usize,
+    to_node: usize,
+) -> Option<(usize, [usize; 2])> {
+    let mut visited = HashMap::new();
+    let mut queue = VecDeque::new();
+    queue.push_back((from_node, 0usize, true));
+    queue.push_back((to_node, 0usize, false));
+
+    while let Some((node, length, forward)) = queue.pop_front() {
+        //visit node
+        match visited.entry((node, forward)) {
+            Entry::Occupied(mut entry) => {
+                let current_length = entry.get_mut();
+                if length < *current_length {
+                    *current_length = length;
+                } else {
+                    continue;
+                }
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(length);
+            }
+        }
+
+        //check reached end
+        if let Entry::Occupied(complement) = visited.entry((node, !forward)) {
+            let mut lengths = [length, *complement.get()];
+            if !forward {
+                lengths.reverse();
+            }
+            return Some((node, lengths));
+        }
+
+        //enque neighbors
+        let neighbors = if forward {
+            adjecancy.row(node)
+        } else {
+            adjecancy.column(node)
+        };
+        for (neighbor, &adjecant) in neighbors.into_iter().enumerate() {
+            if adjecant {
+                queue.push_front((neighbor, length + 1, forward))
+            }
+        }
+    }
+    None
 }
